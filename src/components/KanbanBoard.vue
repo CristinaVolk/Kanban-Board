babeb00a3bfc9a1bf745c1432e78d42aa413ee50
CristinaVolk <kristinavolk4@tut.by>
2020-03-26 21:01:44 +0100

    some styling

--- src/components/KanbanBoard.vue
+++ src/components/KanbanBoard.vue
@@ -2,16 +2,16 @@
 	<div class="board">
 		<menu-bar></menu-bar>
 		<div class="row">
-			<div class="col-md">
+			<div class="col-sm-3">
 				<task-lane id="0" title="on_hold" :items="on_hold"></task-lane>
 			</div>
-			<div class="col-md">
+			<div class="col-sm-3">
 				<task-lane id="1" title="inProgress" :items="inProgress"></task-lane>
 			</div>
-			<div class="col-md">
+			<div class="col-sm-3">
 				<task-lane id="2" title="needs_review" :items="needs_review"></task-lane>
 			</div>
-			<div class="col-md">
+			<div class="col-sm-3">
 				<task-lane id="3" title="approved" :items="approved"></task-lane>
 			</div>
 		</div>
@@ -38,3 +38,18 @@
 		} )
 	};
 </script>
+
+<style>
+	.menubar {
+		margin-bottom: 16px;
+	}
+
+	#space {
+		margin: 20px;
+	}
+
+	.row {
+		display: flex;
+	}
+	
+</style>
--- src/components/TaskLane.vue
+++ src/components/TaskLane.vue
@@ -3,7 +3,7 @@
 		<div class="card-body">
 			<draggable v-model="draggables" :options="{ group: 'default' }">
 				<div v-for="item in items" :key="item.id">
-					<item :item="item"></item>
+					<item id="item-lane" :item="item"></item>
 				</div>
 			</draggable>
 		</div>
@@ -54,5 +54,8 @@
 <style>
 	.card-body>* {
 		min-height: 50px;
+		min-width: 400px;
 	}
+
+	
 </style>
