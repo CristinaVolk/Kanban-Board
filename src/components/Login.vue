03eef0b18027c549706f13d6a30a0aa277700fab
CristinaVolk <kristinavolk4@tut.by>
2020-03-31 21:34:03 +0200

    register link update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,19 @@
 <template>
  <div>
    <form class="login" @submit.prevent="login">
-     <h1>Sign in</h1>
+     <h1>Sign in
+		<router-link to="/register" :class="buttonClass('/register')">
+			Register
+			<i id="space" class="fa fa-th-large" aria-hidden="true"></i>
+		</router-link>
+	 </h1>
      <label>Username</label>
      <input required v-model="username" type="text" placeholder="Name"/>
      <label>Password</label>
      <input required v-model="password" type="password" placeholder="Password"/>
      <hr/>
 	 <button type="submit">Login</button>
-		<span v-if="error">{{error}}
-			<router-link to="/register" :class="buttonClass('/register')">
-				Register
-				<i id="space" class="fa fa-th-large" aria-hidden="true"></i>
-			</router-link>
-			</span>
+		<span v-if="error">{{error}}</span>
    </form>
  </div>
 </template>
@@ -36,17 +36,25 @@
 		   		.then(() => this.$router.push('/board'))
 		   		.catch(err => {
 					   console.log(err)
-					   this.error = `Unable to log in with provided credentials. Please try again or `
+					   this.error = `Unable to log in with provided credentials. Please try again`
 				   })
 			   },
 			   buttonClass( path )
 			{
 				return {
 					btn: true,
-					'btn-primary': this.$route.path === path,
-					'btn-secondary': this.$route.path !== path
+					'btn-primary': this.$route.path === path
 				};
 			}
 		}
 	}
 </script>
+
+<style>
+	label {
+		margin: 5px;
+	}
+	.btn {
+		color: rgb(252, 161, 128);
+	}
+</style>
